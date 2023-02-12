c376d5e3c18206e817127fbf67d480724f081271
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-02-12 18:31:53 +0400

    App func to arrow

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from "./App.module.scss"
 const cx = classNames.bind(styles)
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <div className="App">
-    55
+      
     </div>
   )
 }
